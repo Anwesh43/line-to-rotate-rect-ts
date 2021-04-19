6582fa6fec5530d5c0027351142fe5b40a2e3f99
Anwesh43 <anweshthecool0@gmail.com>
2021-04-19 23:08:44 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const colors : Array<string> = [
 ] 
 const backColor : string = "#BDBDBD"
 const rot : number = Math.PI / 2 
-
+const sizeFactor : number = 6.9 
 class ScaleUtil {
 
     static maxScale(scale : number, i : number, n : number) : number {
@@ -27,5 +27,39 @@ class ScaleUtil {
 
     static sinify(scale : number) : number {
         return Math.sin(scale * Math.PI)       
+    }
+}
+
+class DrawingUtil {
+
+    static drawLine(context  : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineToRotateRect(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        context.save()
+        context.translate(w / 2, h / 2)
+        context.rotate(rot * sf1)
+        for (var j = 0; j < 2; j++) {
+            DrawingUtil.drawLine(context, 0, 0, 0, -size * sf2)
+        }
+        context.fillRect(0, 0, size, -size * sf3)
+        context.restore()
+    }
+
+    static drawLTRRNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineToRotateRect(context, scale)
     }
 }
