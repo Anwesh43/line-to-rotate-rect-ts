004fdc545afe9f186e9bbd4e5ecd834a4073f9ad
Anwesh43 <anweshthecool0@gmail.com>
2021-04-20 02:33:52 +0530

    adding LTRRNode class

--- index.ts
+++ index.ts
@@ -137,3 +137,46 @@ class Animator {
         }
     }
 }
+
+class LTRRNode {
+
+    next : LTRRNode 
+    prev : LTRRNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LTRRNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLTRRNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LTRRNode {
+        var curr : LTRRNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+    
+}
